1a2acaa9264150ec3bf475007af19ac397ecba9e
Cash <cashhilstad@gmail.com>
2023-10-27 16:13:14 -0700

    final

--- main.cpp
+++ main.cpp
@@ -3,19 +3,16 @@
 
 using namespace std;
 
+//change these to change the grid size
 const int WIDTH = 10;
 const int HEIGHT = 7;
 
+//function prototypes
 void gridToOutput(string inputGrid);
-
 int coordToIndex(int x, int y);
-
 string initializeGrid();
-
 bool dropToken(char token, string& grid);
-
 bool checkWin(char token, string& grid);
-
 bool checkWin(char token, string& grid);
 bool checkVertical(char token, string& grid, int x, int y);
 bool checkHorizontal(char token, string& grid, int x, int y);
@@ -23,28 +20,64 @@ bool checkNegativeOne(char token, string& grid, int x, int y);
 bool checkOne(char token, string& grid, int x, int y);
 
 int main() {
-  string grid = initializeGrid();
-  int placement;
-  bool gameOver = false;
-  gridToOutput(grid); 
+    //create grid string
+    string grid = initializeGrid();
 
-  while(!gameOver) {
-    while(!dropToken('X', grid)) {
-        cout << "Oops, you can't place there! Try again." << endl;
-    };
-    gridToOutput(grid);
-    gameOver = checkWin('X', grid);
-    if (gameOver) {
-        continue;
+    //keep track of game state
+    bool gameOver = false;
+
+    //title screen
+    cout << "XXXX | C O N N E C T   F O U R | OOOO" << endl << endl;
+
+    //print out the grid
+    gridToOutput(grid); 
+
+    //gameplay loop. continue until game ends
+    while(!gameOver) {
+
+        //inform player
+        cout << "Player X's turn." << endl;
+
+        //get user to drop in a token. stop them if they do it wrong.
+        while(!dropToken('X', grid)) {
+            cout << "Oops, you can't place there! Try again." << endl;
+        };
+
+        //show new state
+        gridToOutput(grid);
+
+        //check to see if they won
+        gameOver = checkWin('X', grid);
+
+        //if they won, stop the loop.
+        if (gameOver) {
+            cout << "Player X won!" << endl;
+            break;
+        }
+
+        //inform player
+        cout << "Player O's turn." << endl;
+
+        //get user to drop in a token. stop them if they do it wrong.
+        while(!dropToken('O', grid)) {
+            cout << "Oops, you can't place there! Try again." << endl;
+        };
+
+        //show new state
+        gridToOutput(grid);
+
+        //check to see if they won
+        gameOver = checkWin('O', grid);
+
+        //if they won, stop the loop.
+        if (gameOver) {
+            cout << "Player O won!" << endl;
+            break;
+        }
     }
-    while(!dropToken('O', grid)) {
-        cout << "Oops, you can't place there! Try again." << endl;
-    };
-    gridToOutput(grid);
-    gameOver = checkWin('O', grid);
-  }
 }
 
+//calculate the size of the grid string and create it 
 string initializeGrid() {
   string grid = "";
   for (int y = 0; y < HEIGHT; y++) {
@@ -55,6 +88,7 @@ string initializeGrid() {
   return grid;
 }
 
+//print out the grid's contents
 void gridToOutput(string inputGrid) {
   for (int y = HEIGHT - 1; y >= 0; y--) {
     for (int x = 0; x < WIDTH; x++) {
@@ -62,18 +96,25 @@ void gridToOutput(string inputGrid) {
     }
     cout << endl;
   }
+  cout << "0123456789" << endl;
 }
 
+//take a coordinate and convert it to an index in the grid
 int coordToIndex(int x, int y) {
   int index = ((y)*WIDTH);
   index += x;
   return index;
 }
 
+//asks for player input, then drops a token. returns false if it failed.
 bool dropToken(char token, string& grid) {
     int x;
-    cout << "Please enter where you would like to drop a token:";
+    cout << "Please enter where you would like to drop a token: ";
     cin >> x;
+    while (x >= WIDTH) {
+        cout << "Please enter a valid number: ";
+        cin >> x;
+    }
     for (int y = 0; y < HEIGHT; y++) {
         if (grid[coordToIndex(x, y)] == '-') {
             grid[coordToIndex(x,y)] = token;
@@ -83,6 +124,7 @@ bool dropToken(char token, string& grid) {
     return false;
 }
 
+//uses the other check methods to see if someone won
 bool checkWin(char token, string& grid) {
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
@@ -90,12 +132,17 @@ bool checkWin(char token, string& grid) {
                 return true;
             } else if (checkHorizontal(token, grid, x, y)) {
                 return true;
+            } else if (checkNegativeOne(token, grid, x, y)) {
+                return true;
+            } else if (checkOne(token, grid, x, y)) {
+                return true;
             }
         }
     }
     return false;
 }
 
+//checks vertically for a win
 bool checkVertical(char token, string& grid, int x, int y) {
     int streak = 0;
     for (int ySelection = 0; ySelection < HEIGHT; ySelection++) {
@@ -113,6 +160,7 @@ bool checkVertical(char token, string& grid, int x, int y) {
     return false;
 }
 
+//checks horizontally for a win
 bool checkHorizontal(char token, string& grid, int x, int y) {
     int streak = 0;
     for (int xSelection = 0; xSelection < WIDTH; xSelection++) {
@@ -130,19 +178,54 @@ bool checkHorizontal(char token, string& grid, int x, int y) {
     return false;
 }
 
+//checks diagonal down for a win
 bool checkNegativeOne(char token, string& grid, int x, int y) {
     int streak = 0;
-    int xVar, yVar;
+    int steps = 0;
+    int xVar = x;
+    int yVar = y;
 
-    while(xVar >= 0 or yVar < HEIGHT) {
+    while((steps < 4) && ((xVar < WIDTH) && (yVar < HEIGHT))) {
         if (grid[coordToIndex(xVar, yVar)] == token) {
             streak += 1;
         } else {
-            streak -= 1;
+            streak = 0;
         }
+        
+        xVar++;
+        yVar--;
+
+        if (streak >= 4) {
+            return true;
+        }
+
+        steps++;
     }
+    return false;
 }
 
+//checks diagonal up for a win
 bool checkOne(char token, string& grid, int x, int y) {
+    int streak = 0;
+    int steps = 0;
+    int xVar = x;
+    int yVar = y;
 
+    while((steps < 4) && ((xVar < WIDTH) && (yVar < HEIGHT))) {
+        if (grid[coordToIndex(xVar, yVar)] == token) {
+            streak += 1;
+        } else {
+            streak = 0;
+        }
+        
+        xVar++;
+        yVar++;
+
+        if (streak >= 4) {
+            return true;
+        }
+
+        steps++;
+    }
+    return false;
 }
